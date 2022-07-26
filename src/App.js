8a1a9b628e534014281a0f75f4a0a6ca41bd9257
prerna-gupta02 <engineerprerna02@gmail.com>
2022-07-26 19:45:37 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { useState } from 'react';
+import mimg from './assets/cone.png';
+
+function App() {
+  const [modal, setModal] = useState(false);
+  const toggle = () => {
+    setModal(!modal);
+  }
+  return (
+    <div className="App">
+      <Button outline onClick={toggle}>Order</Button>
+        <Modal isOpen={modal} toggle={toggle} className="mmain">
+          <ModalBody className='mbody'>
+            <img src={mimg} />
+            <h6>Your order is completed!</h6>
+            <p>You will be receiving a confirmation email with order details.</p>
+            <Button className='bttn' onClick={toggle}>Go back to homepage</Button>{' '}
+          </ModalBody>
+        </Modal>
+    </div>
+  );
+}
+
+export default App;
